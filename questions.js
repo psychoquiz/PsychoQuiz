104f3b03d190e8c5fcb62394eb927baa169b3db3
psychoquiz <succoallafragola@gmail.com>
2026-04-06 16:49:10 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -3,12 +3,12 @@ const questionsBySubject = {
     {
       domanda: "Qual è la differenza tra conoscenze dichiarative e conoscenze procedurali?",
       risposte: [
-        "Le conoscenze dichiarative riguardano il “come fare” un'azione (es. digitare su una tastiera), quelle procedurali riguardano fatti e informazioni",
-        "Le conoscenze dichiarative riguardano fatti e informazioni, quelle procedurali riguardano il “come fare” un’azione (es. digitare su una tastiera)",
+        "Le conoscenze dichiarative riguardano il 'come fare' un'azione (es. digitare su una tastiera), quelle procedurali riguardano fatti e informazioni",
+        "Le conoscenze dichiarative riguardano fatti e informazioni, quelle procedurali riguardano il 'come fare' un'azione (es. digitare su una tastiera)",
         "Le conoscenze dichiarative sono inconsce, quelle procedurali sono consapevoli",
-        "Non c’è alcuna differenza, entrambe sono sempre accessibili alla consapevolezza e verbalizzabili"
+        "Non c'è alcuna differenza, entrambe sono sempre accessibili alla consapevolezza e verbalizzabili"
       ],
-      corretta: 2
+      corretta: 1
     }
   ],
 
